877e1a2a725801de2635bf681e5f00a74dc9abd0
Elor-Itz <elor2200@gmail.com>
2025-02-25 15:52:43 +0200

    Game menu now uses modal hook

--- src/components/HexGameMenu.jsx
+++ src/components/HexGameMenu.jsx
@@ -1,58 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import GameSetup from './GameSetup';
 import OptionsModal from './OptionsModal';
-import useOptions from '../hooks/useOptions';
 import HowToPlayModal from './HowToPlayModal';
+import useModal from '../hooks/useModal';
+import useOptions from '../hooks/useOptions';
 import '../styles/HexGameMenu.css';
 
 // HexGameMenu component
 const HexGameMenu = ({ onStartGame }) => {
     // Menu modals
-    const [showGameSetup, setShowGameSetup] = useState(false);
-    const [showOptions, setShowOptions] = useState(false);
-    const { loadOptions } = useOptions();
-    const [showHowToPlay, setShowHowToPlay] = useState(false);  
+    const gameSetupModal = useModal();
+    const optionsModal = useModal();
+    const howToPlayModal = useModal();
+    const { loadOptions } = useOptions();     
 
     // Load stored options when the component mounts
     useEffect(() => {
         loadOptions();
-    }, []);
-
-    // Handle new game button click
-    const handleNewGame = () => {
-        setShowGameSetup(true);
-    };
-    
-    // Handle return button click
-    const handleReturn = () => {
-        setShowGameSetup(false);
-    };
-    
-    // Handle new game button click
-    const handleOptions = () => {  
-        setShowOptions(true);      
-    };
-
-    // Handle instructions button click
-    const handleShowHowToPlay = () => {
-        setShowHowToPlay(true);        
-    };
-    
-    // Handle modal close
-    const handleCloseModal = () => {        
-        setShowOptions(false);
-        setShowHowToPlay(false);
-    };    
+    }, []);     
     
     // Close modals on Esc key press
     useEffect(() => {
         const handleKeyDown = (event) => {
             if (event.key === 'Escape') {
                 document.activeElement.blur();
-                if (showOptions || showHowToPlay) {
-                    handleCloseModal();
-                } else if (showGameSetup) {
-                    handleReturn();
+                if (optionsModal.isVisible || howToPlayModal.isVisible) {
+                    optionsModal.close();
+                    howToPlayModal.close();
+                } else if (gameSetupModal.isVisible) {
+                    gameSetupModal.close();
                 }
             }
         };
@@ -61,25 +37,25 @@ const HexGameMenu = ({ onStartGame }) => {
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
         };
-    }, );
+    }, [gameSetupModal.isVisible, optionsModal.isVisible, howToPlayModal.isVisible]);
 
     return (
         <div id="menu-wrapper">
-            {!showGameSetup ? (
+            {!gameSetupModal.isVisible ? (
                 <div id="menu-container">
                     <h1 className="flash">Hex</h1>
-                    <button onClick={handleNewGame}>New Game</button>
-                    <button onClick={handleOptions}>Options</button> 
-                    <button onClick={handleShowHowToPlay}>How to Play</button>                                       
+                    <button onClick={gameSetupModal.open}>New Game</button>
+                    <button onClick={optionsModal.open}>Options</button> 
+                    <button onClick={howToPlayModal.open}>How to Play</button>                                       
                 </div>
             ) : (
-                <GameSetup onStartGame={onStartGame} onReturn={handleReturn} />
+                <GameSetup onStartGame={onStartGame} onReturn={gameSetupModal.close} />
             )}            
-            {showOptions && (
-                <OptionsModal show={showOptions} onClose={handleCloseModal} />                
+            {optionsModal.isVisible && (
+                <OptionsModal show={optionsModal.isVisible} onClose={optionsModal.close} />                
             )}
-            {showHowToPlay && (
-                <HowToPlayModal show={showHowToPlay} onClose={handleCloseModal} />
+            {howToPlayModal.isVisible && (
+                <HowToPlayModal show={howToPlayModal.isVisible} onClose={howToPlayModal.close} />
             )}            
         </div>
     );
